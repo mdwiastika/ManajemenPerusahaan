8f358a6d41b660ac8a3c23cd6e9dd012983e8594
Marcel Dwi Astika <marceldwias@gmail.com>
2024-10-07 09:10:15 +0700

    update: api versioning

--- routes/api.php
+++ routes/api.php
@@ -9,26 +9,28 @@ use App\Http\Controllers\Api\KaryawanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
 
 
-Route::apiResource('/karyawans', KaryawanController::class);
-Route::apiResource('/departemens', DepartemenController::class);
-Route::apiResource('/jabatans', JabatanController::class);
-Route::apiResource('/absensis', AbsensiController::class);
-Route::apiResource('/gajis', GajiController::class);
-Route::prefix('auth')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-});
+    Route::apiResource('/karyawans', KaryawanController::class);
+    Route::apiResource('/departemens', DepartemenController::class);
+    Route::apiResource('/jabatans', JabatanController::class);
+    Route::apiResource('/absensis', AbsensiController::class);
+    Route::apiResource('/gajis', GajiController::class);
+    Route::prefix('auth')->group(function () {
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    });
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('/karyawans', KaryawanController::class)->only(['store', 'update', 'destroy']);
-    Route::apiResource('/departemens', DepartemenController::class)->only(['store', 'update', 'destroy']);
-    Route::apiResource('/jabatans', JabatanController::class)->only(['store', 'update', 'destroy']);
-    Route::apiResource('/absensis', AbsensiController::class)->only(['store', 'update', 'destroy']);
-    Route::apiResource('/gajis', GajiController::class)->only(['store', 'update', 'destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::apiResource('/karyawans', KaryawanController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/departemens', DepartemenController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/jabatans', JabatanController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/absensis', AbsensiController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('/gajis', GajiController::class)->only(['store', 'update', 'destroy']);
+    });
 });
